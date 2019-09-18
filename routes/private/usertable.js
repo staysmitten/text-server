027e43a62a2a408cf1b720bd744db1290e258f58
Devon Marantz <devonmarantz@gmail.com>
2019-09-18 10:01:29 -0700

    Fix usertable route

--- routes/private/usertable.js
+++ routes/private/usertable.js
@@ -2,6 +2,7 @@
 const router = require('express').Router();
 const jwt = require('jsonwebtoken');
 const verifyToken = require('../../middleware/verifyToken');
+const userController = require('../../controllers/user.controller');
 
 
 /**
@@ -12,14 +13,29 @@ router.get('/',
   verifyToken,
   (req, res) => {  
     jwt.verify(req.token, process.env.JWT_SECRET, async (err, authData) => {
-      try{
+      // try{
+      //   const users = await userController.readMany();
+      //   return res.status(201).json(users);
+      // } catch(err){
+      //   res.sendStatus(403);
+      // }
+      if(err) {
+        res.sendStatus(403);
+      } else {
         const users = await userController.readMany();
         return res.status(201).json(users);
-      } catch(err){
-        res.sendStatus(403);
       }
     });
   }
   );
+
+// // Route to use once authentication is working
+// router.get(
+//   '/',
+//   asyncWrapper(async (req, res) => {
+//     const users = await userController.readMany();
+//     return res.status(201).json(users);
+//   })
+// );
 
 module.exports = router;
