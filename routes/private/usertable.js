795e32f74aeeba7c63e9fd25cdf5e91dc36e72e7
Devon Marantz <devonmarantz@gmail.com>
2019-09-18 09:57:22 -0700

    Fix issuer and have private route return users

--- routes/private/usertable.js
+++ routes/private/usertable.js
@@ -11,27 +11,15 @@ const verifyToken = require('../../middleware/verifyToken');
 router.get('/', 
   verifyToken,
   (req, res) => {  
-    jwt.verify(req.token, process.env.JWT_SECRET, (err, authData) => {
-      if(err) {
+    jwt.verify(req.token, process.env.JWT_SECRET, async (err, authData) => {
+      try{
+        const users = await userController.readMany();
+        return res.status(201).json(users);
+      } catch(err){
         res.sendStatus(403);
-      } else {
-        res
-          .status(200)
-          .send(
-            'This is a protected route. You should only see this if you are authenticated in.'
-          );
       }
     });
   }
   );
-
-// // Route to use once authentication is working
-// router.get(
-//   '/',
-//   asyncWrapper(async (req, res) => {
-//     const users = await userController.readMany();
-//     return res.status(201).json(users);
-//   })
-// );
 
 module.exports = router;
