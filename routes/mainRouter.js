6d589f455788049b3c385ca2b28db276beb78e8a
Devon Marantz <devonmarantz@gmail.com>
2019-09-18 12:16:59 -0700

    Remove private route in router

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -7,14 +7,12 @@ const express = require('express');
 const indexRouter = require('./index');
 const userRouter = require('./users');
 const authRouter = require('./auth');
-const privateRoute = require('./private/usertable');
 
 const app = express();
 
 // subrouters
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
-app.use('/api/user/admin/', privateRoute);
 app.use('/', indexRouter);
 
 module.exports = app;
